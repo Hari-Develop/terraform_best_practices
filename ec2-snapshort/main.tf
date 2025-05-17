c6584b863871ac22fb8a45181c9406ab5d83e23e
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-05-17 16:51:48 +0530

    terrform file chnages

--- ec2-snapshort/main.tf
+++ ec2-snapshort/main.tf
@@ -1,6 +1,6 @@
 # Call the module to create a snapshot of the root EBS volume
 module "snapshot_root_volume" {
     source        = "./modules/snapshot"
-    volume_id     = data.aws_instance.instance_id.root_block_device[0].volume_id
+    volume_id     = element(data.aws_instance.instance_id.root_block_device, 0).volume_id
     env = "prod"
 }
