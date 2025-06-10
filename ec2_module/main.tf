cf631822f5c5756c7482a372e7c845d563205c8e
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-06-10 06:39:09 +0530

    file changes

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -10,7 +10,7 @@ module "route_53" {
   for_each = var.instances
   instances = each.key
   zone_id = var.zone_id
-  records = [module.ec2_instances[each.key].private_ip]
+  records = [module.ec2_instances.private_ip[each.key]]
 }
 
 module "aws_ssm_parameter" {
