41577933d60e2c359944f6cd6252c928cfcba689
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-06-11 04:27:40 +0530

    file changes

--- ec2_module/module/ec2/output.tf
+++ ec2_module/module/ec2/output.tf
@@ -1,14 +1,8 @@
 output "instance_id" {
-  value = {
-    for name, inst in aws_instance.aws_ec2_instances :
-    name => inst.id
-  }
+  value = aws_instance.aws_ec2_instances.id
 }
 
 output "private_ip" {
-  value = {
-    for name, instance in aws_instance.aws_ec2_instances :
-    name => instance.private_ip
-    }
+  value = aws_instance.aws_ec2_instances.private_ip
   }
 
