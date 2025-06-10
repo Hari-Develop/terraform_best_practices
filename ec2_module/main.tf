171313f2687049a931ede0830a2489afdd4df9cc
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-06-10 07:17:52 +0530

    file changes

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -15,7 +15,7 @@ module "route_53" {
 
 module "aws_ssm_parameter" {
   source = "./module/aws_parameres"
-  for_each = module.route_53.aws_route53_record
+  for_each = module.route_53
   name = "/${var.env}/${each.key}/dns"
   description = "The public DNS name for NGINX load balancer"
   type = "String"
