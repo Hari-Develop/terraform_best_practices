4bc9671cb5e3e99079a4ccaa76c052901152c030
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-06-10 08:29:42 +0530

    file changes

--- ec2_module/module/route_53/output.tf
+++ ec2_module/module/route_53/output.tf
@@ -1,7 +1,7 @@
 output "route53_record_names" {
   description = "Map of instance key → Route53 record name"
   value = {
-    for inst_key, mod in module.route_53 :
+    for inst_key, mod in aws_route53_record.route_53 :
     inst_key => mod.record_name
   }
 }
