17de5af6c6ebbc2a04d1f21fca4e80dea943bb2c
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2025-06-10 22:49:04 +0530

    file changes

--- ec2_module/output.tf
+++ ec2_module/output.tf
@@ -2,6 +2,6 @@ output "ec2_instances_id" {
   value = module.ec2_instances.instance_id
 }
 
-output "dns_record"{
-  value = module.route_53.name
+output "dns_record" {
+  value = { for k, mod in module.route_53 : k => mod.name }
 }
