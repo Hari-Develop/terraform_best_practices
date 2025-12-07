59b517a3b7543d3715b035d0a009badd38fc3699
Harinadha Reddy <Harinadha.Reddy@tmdc.io>
2025-12-07 16:07:43 +0530

    adding the file

--- route53_auto_record/data.tf
+++ route53_auto_record/data.tf
@@ -1,4 +1,13 @@
 data "aws_ami" "redhat" {
-  owners =["RHEL-9-DevOps-Practice"]
+  owners =["973714476881"]
   most_recent = true
+   filter {
+    name   = "name"
+    values = ["RHEL-9*"]
+  }
+  
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
